717d32126af6df6205d7fd338f0f2b7d1539eea1
Samuel Muigai <ndungusam01@gmail.com>
2024-03-12 17:11:50 +0300

    Restyled the login form

--- client/src/Landing Page/Login/Login.js
+++ client/src/Landing Page/Login/Login.js
@@ -65,24 +65,24 @@ const Login = () =>
             )
     }
     return ( 
-        <div className='background d-flex justify-content-end align-items-center'>
-            <Form className='form mx-4 mt-4 bg-light' onSubmit={formSubmit}>
-                <img src={MANImage} alt='Logo' className='img-thumbnail mb-3 border border-0'/>
-                <Form.Group className='mx-2 mb-2'>
-                    <Form.Label className='fs-5'>Username</Form.Label>
-                    <Form.Control type='username' id="username" value={loginInfo.username} onChange={handleInputChange} placeholder='Enter your username' required></Form.Control>
+        <div className="background d-flex justify-content-end align-items-start">
+            <Form className="bg-light form mx-3 p-1" onSubmit={formSubmit}>
+                <img src={MANImage} alt="Mobikey Logo" className="img-thumbnail mb-3 border border-0" />
+                <Form.Group className="mx-2 mb-2">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="name" id="username" value={loginInfo.username} onChange={handleInputChange} placeholder="Enter your username" required></Form.Control>
                 </Form.Group>
-                <Form.Group className='mx-2 mb-3'>
-                    <Form.Label className='fs-5'>Password</Form.Label>
-                    <Form.Control type='password' id="password" value={loginInfo.password} onChange={handleInputChange} placeholder='Enter your password' required></Form.Control>
+                <Form.Group className="mx-2 mb-2">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" id="password" value={loginInfo.password} onChange={handleInputChange} placeholder="Enter your password" required></Form.Control>
                 </Form.Group>
-                <div className="mt-3 buttons">
-                    <Button variant='success' type='submit' className='col-md-5 ms-2'>Login</Button>
-                    <Link to="/" variant='danger' className='btn btn-danger col-md-5 ms-5 me-2'>Back</Link>
+                <div className="d-flex justify-content-between gap-5 mt-3 px-2">
+                    <Button type="submit" variant="success" className="col-md-5">Login</Button>
+                    <Link className="btn btn-danger col-md-5" to="/">Cancel</Link>
                 </div>
-                <div className="container">
-                    <Link to="/reset" className='btn btn-danger col-md-12 mx-auto my-3'>Forgot password?</Link>
-                </div> 
+                <div className='px-2 my-2'>
+                    <Link className="btn btn-danger col-12" to="/reset">Forgot password?</Link>
+                </div>
             </Form>
         </div>
      );
